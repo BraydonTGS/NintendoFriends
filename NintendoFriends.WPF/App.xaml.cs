3ded8cf9d4c952f1f2b9143537b90bf153d0a39e
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-04-10 18:59:21 -0500

    Scaffolded out User Interface Rough Draft

--- NintendoFriends.WPF/App.xaml.cs
+++ NintendoFriends.WPF/App.xaml.cs
@@ -5,5 +5,11 @@ namespace NintendoFriends.WPF
 
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
---
+++ NintendoFriends.WPF/Views/NintendoFriendsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace NintendoFriends.WPF.Views
+{
+    public partial class NintendoFriendsView : UserControl
+    {
+        public NintendoFriendsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
