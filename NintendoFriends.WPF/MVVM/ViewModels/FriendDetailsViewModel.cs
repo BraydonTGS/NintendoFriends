9fefc7fad6c3db339055232e432d4290e520eb0e
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-04-16 15:03:21 -0500

    Hooked up datacontext to the FriendDetailsView

--- NintendoFriends.WPF/MVVM/ViewModels/FriendDetailsViewModel.cs
+++ NintendoFriends.WPF/MVVM/ViewModels/FriendDetailsViewModel.cs
@@ -2,5 +2,18 @@
 {
    public class FriendDetailsViewModel : ViewModelBase
     {
+        public string Username { get; }
+        public string IsBestFriendDisplay { get; }
+        public string IsOnlineDisplay { get; }
+        public string FavoriteGame { get; }
+
+        public FriendDetailsViewModel()
+        {
+            Username = "GeoMatics";
+            IsBestFriendDisplay = "Yes";
+            IsOnlineDisplay = "No";
+            FavoriteGame = "Breath of the Wild";
+        }
+
     }
 }
