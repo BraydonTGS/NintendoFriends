1e44ed75f8cebeb21a3de2ec951fc4707f429ea1
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-04-16 14:54:54 -0500

    Created the FriendUsername Viewmodel and styled the FriendsList Component

---
+++ NintendoFriends.WPF/MVVM/ViewModels/FriendUsernameViewModel.cs
@@ -0,0 +1,19 @@
+﻿using NintendoFriends.WPF.MVVM.ViewModels;
+using System.Windows.Input;
+
+namespace NintendoFriends.WPF.MVVM.Components
+{
+    public class FriendUsernameViewModel : ViewModelBase
+    {
+        public string Username { get; }
+
+        // Commands //
+        public ICommand EditCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public FriendUsernameViewModel(string userName)
+        {
+            Username = userName;
+        }
+    }
+}
--- NintendoFriends.WPF/MVVM/ViewModels/FriendsListViewModel.cs
+++ NintendoFriends.WPF/MVVM/ViewModels/FriendsListViewModel.cs
@@ -1,6 +1,28 @@
-﻿namespace NintendoFriends.WPF.MVVM.ViewModels
+﻿using NintendoFriends.WPF.MVVM.Components;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace NintendoFriends.WPF.MVVM.ViewModels
 {
     public class FriendsListViewModel : ViewModelBase
     {
+        private readonly ObservableCollection<FriendUsernameViewModel> _friendListingViewModels;
+        public IEnumerable<FriendUsernameViewModel> FriendListingViewModels => _friendListingViewModels;
+
+        public FriendsListViewModel()
+        {
+            _friendListingViewModels = new ObservableCollection<FriendUsernameViewModel>
+            {
+                new FriendUsernameViewModel("GeoMatics"),
+                new FriendUsernameViewModel("StepDown"),
+                new FriendUsernameViewModel("RageBlast"),
+                new FriendUsernameViewModel("GeoMatics"),
+                new FriendUsernameViewModel("StepDown"),
+                new FriendUsernameViewModel("RageBlast"),
+                new FriendUsernameViewModel("GeoMatics"),
+                new FriendUsernameViewModel("StepDown"),
+                new FriendUsernameViewModel("RageBlast"),
+            };
+        }
     }
 }
