75f7300644e4d2a4daf97886961caaadd8642c10
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-04-27 19:51:11 -0500

    Created new ViewModels for the Add and Edit Modal

---
+++ NintendoFriends.WPF/MVVM/ViewModels/AddFriendViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NintendoFriends.WPF.MVVM.ViewModels
+{
+    public class AddFriendViewModel : ViewModelBase 
+    {
+        public FriendDetailsFormViewModel Form { get; set; }
+        public AddFriendViewModel()
+        {
+            Form = new FriendDetailsFormViewModel();
+        }
+    }
+}
---
+++ NintendoFriends.WPF/MVVM/ViewModels/EditFriendViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace NintendoFriends.WPF.MVVM.ViewModels
+{
+    public class EditFriendViewModel : ViewModelBase
+    {
+        public FriendDetailsFormViewModel Form { get; set; }
+
+        public EditFriendViewModel()
+        {
+            Form = new FriendDetailsFormViewModel(); 
+        }
+    }
+}
